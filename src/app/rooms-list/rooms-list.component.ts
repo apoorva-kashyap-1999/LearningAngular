6405ed8a6bb26efa4770727248e54bbb4a25d1a2
Apoorva <apkpashu@gmail.com>
2024-05-05 20:32:15 +0530

    onChanges, ChangeDetection

--- src/app/rooms-list/rooms-list.component.ts
+++ src/app/rooms-list/rooms-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { ChangeDetectionStrategy, Component, EventEmitter, Input, OnChanges, OnInit, Output, SimpleChanges } from '@angular/core';
 import { Room, RoomData } from '../rooms/rooms';
 import { emit } from 'process';
 
@@ -6,15 +6,21 @@ import { emit } from 'process';
   selector: 'app-rooms-list',
   templateUrl: './rooms-list.component.html',
   styleUrl: './rooms-list.component.css',
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class RoomsListComponent implements OnInit {
+export class RoomsListComponent implements OnInit, OnChanges {
   @Input() rooms: RoomData[] = [];
+  @Input() title:string='';
 
   @Output() selectedRoom = new EventEmitter<RoomData>();
 
   constructor() {}
 
   ngOnInit(): void {}
+
+  ngOnChanges(changes: SimpleChanges): void {
+    console.log(changes);
+  }
 
   selectRoom(room : RoomData){
     this.selectedRoom.emit(room);
--- src/app/rooms/rooms.component.ts
+++ src/app/rooms/rooms.component.ts
@@ -1,49 +1,66 @@
 import { Component, OnInit } from '@angular/core';
-import { Room , RoomData } from './rooms';
+import { Room, RoomData } from './rooms';
 
 @Component({
   selector: 'app-rooms',
   templateUrl: './rooms.component.html',
-  styleUrl: './rooms.component.css'
+  styleUrl: './rooms.component.css',
 })
 export class RoomsComponent implements OnInit {
+  constructor() {}
 
-  hotelName='Taj Hotel'
-  noOfRooms=20
-  hideRooms= false
-  selectedRoom = {}
+  hotelName = 'Taj Hotel';
+  noOfRooms = 20;
+  hideRooms = false;
+  selectedRoom = {};
+  title = 'Rooms-List';
   e: any;
 
-  toggle(){
+  toggle() {
+    this.title = 'Find Rooms';
+  }
+
+  showRooms() {
     this.hideRooms = !this.hideRooms;
   }
-  
-  selectRoom(room :RoomData){
+
+  selectRoom(room: RoomData) {
     console.log(room);
-    this.selectedRoom=room;
+    this.selectedRoom = room;
   }
-  
-  room : Room = {
-      totalRooms: 20,
-     availableRooms : 8,
-     bookedRooms : 12
-  }
-  roomsList : RoomData[]=[];
-  constructor(){ }
 
-  ngOnInit(): void { 
-    this.roomsList = [{
-      roomType:"Deluxe",
-      amenities:"AC",
-      price:2000,
-      checkIn:new Date()
-     },{
-       roomType:"Suite",
-       amenities:"Jacuzi",
-       price:5000,
-       checkIn:new Date()
-     }]
-   }
+  addRoom() {
+    const NewRoom: RoomData = {
+      roomType: 'AddNewRoomSuite',
+      amenities: 'AC',
+      price: 10000,
+      checkIn: new Date(),
+    };
+    this.roomsList = [...this.roomsList, NewRoom]; //   // Create a new array reference for table so table detects changes
+    //  this.roomsList.push(NewRoom);
+  }
+
+  room: Room = {
+    totalRooms: 20,
+    availableRooms: 8,
+    bookedRooms: 12,
+  };
+  roomsList: RoomData[] = [];
+
+  ngOnInit(): void {
+    this.roomsList = [
+      {
+        roomType: 'Deluxe',
+        amenities: 'AC',
+        price: 2000,
+        checkIn: new Date(),
+      },
+      {
+        roomType: 'Suite',
+        amenities: 'Jacuzi',
+        price: 5000,
+        checkIn: new Date(),
+      },
+    ];
+  }
 }
-
-
